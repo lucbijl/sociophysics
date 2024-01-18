90c034b64a91f43267e3d22b7df5ea4ec8a0235c
Luc <luc@bijl.us>
2024-01-18 10:01:27 +0100

    Big update.

--- sentiment-analysis/scripts/emotion-analysis.py
+++ sentiment-analysis/scripts/emotion-analysis.py
@@ -6,28 +6,29 @@ from torch.utils.data import DataLoader
 from transformers import DistilBertForSequenceClassification
 from tqdm import tqdm
 
-# Importing the csv dataset
-import_dataset = '../sentiment-model/test-posts-scores.csv'
+# Importing the csv dataset.
+import_dataset = 'datasets/ishw-case/scored/isreal-scored.csv'
 df_data = pd.read_csv(import_dataset)
+print(len(df_data))
 
 # Preparing the dataset for the network this includes tokenization, encoding and creating dataloaders.
 model_name = 'distilbert-base-uncased'
 tokenizer = DistilBertTokenizerFast.from_pretrained(model_name)
 
-encodings = tokenizer(df_data['Text'].tolist(), truncation=True, padding=True, return_tensors='pt')
+encodings = tokenizer(df_data['text'].tolist(), truncation=True, padding=True, return_tensors='pt')
 
 dataset = torch.utils.data.TensorDataset(
     encodings['input_ids'], 
     encodings['attention_mask']
 )
-dataloader = DataLoader(dataset, batch_size=6, shuffle=False)
+dataloader = DataLoader(dataset, batch_size=32, shuffle=False)
 
-# Loading the model
-model= torch.load('bert-go-emotion.pth')
+# Loading the model.
+model= torch.load('sentiment-analysis/models/bert-go-emotion.pth')
 
-# Using the model to perform emotion analysis on the dataset
+# Using the model to perform emotion analysis on the dataset.
 model.eval()
-list_predicted_scores = {'V': [], 'A': [], 'D': []}
+list_predicted_scores = {'v': [], 'a': [], 'd': []}
 
 for batch in tqdm(dataloader):
     with torch.no_grad():
@@ -38,14 +39,14 @@ for batch in tqdm(dataloader):
         predicted_scores = output.logits
 
         # Writing the emotion scores to the list
-        list_predicted_scores['V'].extend(predicted_scores[:, 0].tolist())
-        list_predicted_scores['A'].extend(predicted_scores[:, 1].tolist())
-        list_predicted_scores['D'].extend(predicted_scores[:, 2].tolist())
+        list_predicted_scores['v'].extend(predicted_scores[:, 0].tolist())
+        list_predicted_scores['a'].extend(predicted_scores[:, 1].tolist())
+        list_predicted_scores['d'].extend(predicted_scores[:, 2].tolist())
 
-# Inserting the emotion scores in the dataset
-for i in ['V', 'A', 'D']:
+# Inserting the emotion scores in the dataset.
+for i in ['v', 'a', 'd']:
     df_data[i] = list_predicted_scores[i]
 
-# Exporting the dataframe to a csv dataset
-export_dataset = '../sentiment-model/test-posts-scores.csv'
+# Exporting the dataframe to a csv dataset.
+export_dataset = 'datasets/ishw-case/scored/israel-scored.csv'
 df_data.to_csv(export_dataset, index=False)
--- sentiment-analysis/scripts/sentiment-analysis.py
+++ sentiment-analysis/scripts/sentiment-analysis.py
@@ -6,26 +6,27 @@ from torch.utils.data import DataLoader
 from transformers import DistilBertForSequenceClassification
 from tqdm import tqdm
 
-# Importing the csv dataset
-import_dataset = '../sentiment-model/test-posts.csv'
+# Importing the csv dataset.
+import_dataset = 'datasets/ishw-case/mastodon/mastodon-israel-cleaned.csv'
 df_data = pd.read_csv(import_dataset)
+print(len(df_data))
 
 # Preparing the dataset for the network this includes tokenization, encoding and creating dataloaders.
 model_name = 'distilbert-base-uncased'
 tokenizer = DistilBertTokenizerFast.from_pretrained(model_name)
 
-encodings = tokenizer(df_data['Text'].tolist(), truncation=True, padding=True, return_tensors='pt')
+encodings = tokenizer(df_data['text'].tolist(), truncation=True, padding=True, return_tensors='pt')
 
 dataset = torch.utils.data.TensorDataset(
     encodings['input_ids'], 
     encodings['attention_mask']
 )
-dataloader = DataLoader(dataset, batch_size=6, shuffle=False)
+dataloader = DataLoader(dataset, batch_size=32, shuffle=False)
 
-# Loading the model
-model= torch.load('bert-imdb.pth')
+# Loading the model.
+model= torch.load('sentiment-analysis/models/bert-imdb.pth')
 
-# Using the model to perform sentiment analysis on the dataset
+# Using the model to perform sentiment analysis on the dataset.
 model.eval()
 list_predicted_scores = []
 
@@ -33,16 +34,16 @@ for batch in tqdm(dataloader):
     with torch.no_grad():
         input_ids, attention_mask = batch
 
-        # Obtaining the sentiment score
+        # Obtaining the sentiment score.
         output = model(input_ids=input_ids, attention_mask=attention_mask)
         predicted_scores = output.logits.view(-1)
 
-        # Writing the sentiment score to the list
+        # Writing the sentiment score to the list.
         list_predicted_scores.extend(predicted_scores.tolist())
 
-# Inserting the sentiment score in the dataset
-df_data['S'] = list_predicted_scores
+# Inserting the sentiment score in the dataset.
+df_data['s'] = list_predicted_scores
 
-# Exporting the dataframe to a csv dataset
-export_dataset = '../sentiment-model/test-posts-scores.csv'
+# Exporting the dataframe to a csv dataset.
+export_dataset = 'datasets/ishw-case/scored/isreal-scored.csv'
 df_data.to_csv(export_dataset, index=False)
